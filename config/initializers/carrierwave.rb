23924f896e3ccdaf5567234f9b65cdb3b7aac94d
asonas <hzw1258@gmail.com>
2015-02-01 03:06:11 +0900

    Using fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,10 @@
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1',
-    }
-    config.fog_directory = 'kitchen'
-    config.fog_public    = false
-  end
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1',
+  }
+  config.fog_directory = Rails.application.secrets.fog_directory
+  config.fog_public    = false
 end
